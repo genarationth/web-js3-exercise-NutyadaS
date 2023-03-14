1b4c4654c708f83643f4df84d2f4f7d5dfe3e3b7
NutyadaS <nutyada.s7810@gmail.com>
2023-03-14 17:35:16 +0700

    edit to be function

--- script.js
+++ script.js
@@ -7,19 +7,50 @@ console.log(monthlyPayment(2500, 0.01, 3));
 
 // Exercise #2
 // #### Part 1
-const greeting = ((name1 = "John"), (name2 = "Smith"), (name3 = "Lieu"));
-{
-  console.log("Welcome " + name1 + ", " + name2 + ", " + name3 + ".");
-}
+const greeting = {
+  nameOne: "John",
+  nameTwo: "Smith",
+  nameThree: "Lieu",
+};
 
 // #### Part 2
-const johnAges = (currentYear, birthYear) => currentYear - birthYear;
-console.log(johnAges(2023, 1990));
-
-const smithAges = (currentYear, birthYear) => currentYear - birthYear;
-console.log(smithAges(2023, 1995));
-
-const lieuAges = (currentYear, birthYear) => currentYear - birthYear;
-console.log(lieuAges(2023, 1999));
-
+const friendsAge = {
+  nameOneAge: 2023 - 1990,
+  nameTwoAge: 2023 - 1995,
+  nameThreeAge: 2023 - 1999,
+};
 // #### Part 3
+
+const introFriend = () => {
+  console.log(
+    "Welcome " +
+      greeting.nameOne +
+      ", you are " +
+      friendsAge.nameOneAge +
+      ". Welcome " +
+      greeting.nameTwo +
+      ", you are " +
+      friendsAge.nameTwoAge +
+      ". Welcome " +
+      greeting.nameThree +
+      ", you are " +
+      friendsAge.nameThreeAge +
+      "."
+  );
+};
+introFriend();
+
+// #### Challenge Yourself
+// #### Part 1
+// function studentsGrade() {
+//   let grade = 5;
+//   if (score >= 5) {
+//     grade = "true";
+//   } else if (score > 8) {
+//     grade = "Excellent";
+//   } else if (score === 11) {
+//     grade = "Perfect";
+//   }
+//   return grade;
+// }
+// console.log(studentsGrade());
