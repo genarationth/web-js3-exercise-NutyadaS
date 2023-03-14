f83c58a30f1e71bf6bcd0eafe460cbe332fd835e
NutyadaS <nutyada.s7810@gmail.com>
2023-03-14 14:14:07 +0700

    add exercise 2 part 1

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 const monthlyPayment = (totalCost, interestFee, fee) =>
   totalCost + totalCost * interestFee + fee;
 
-console.log(monthlyPayment(4784, 0.01, 3));
+console.log(monthlyPayment(2500, 0.01, 3));
 // payment = costs + (costs * 0.01) + 3
 
 // Exercise #2
 // #### Part 1
+const greeting = ((name1 = "John"), (name2 = "Smith"), (name3 = "Lieu"));
+{
+  console.log("Welcome " + name1 + ", " + name2 + ", " + name3 + ".");
+}
